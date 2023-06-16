9fcd59cb979f67ac29322567b44eeba9adfec89a
Keanu D?lle <ares@anghenfil.de>
2023-06-16 23:18:09 +0200

    FEA: download whole categories

--- wp2latex.py
+++ wp2latex.py
@@ -1,5 +1,8 @@
+import os
+import shutil
 import wp_import
 import argparse
+
 
 def main():
     parser = argparse.ArgumentParser(prog="wp2latex", usage="Convert your wordpress blog posts to LaTeX projects")
@@ -7,14 +10,85 @@ def main():
     parser.add_argument('--endnotes', action='store_true')
     parser.add_argument('--first-letter-before')
     parser.add_argument('--first-letter-after')
+    parser.add_argument('--project-template')
+    parser.add_argument('--output')
     parser.add_argument('uris', nargs='+')
     args = parser.parse_args()
 
-    for url in args.uris:
-        print("Downloading "+url)
-        host,slug = wp_import.get_host_slug_from_url(url)
-        post_data = wp_import.import_post(host, slug, args)
-        post_in_latex = wp_import.generate_post(post_data)
+    if len(args.uris) > 1:
+        if not args.project_template:
+            print("Error: Project template required if more than one uri supplied!")
+            exit(-1)
+
+        # Generate output path
+        output_path = ""
+        if args.output:
+            output_path = args.output
+        else:
+            if not os.path.exists("output"):
+                output_path = "output"
+            else:
+                x = 1
+                while os.path.exists("output_" + str(x)):
+                    x = x + 1
+
+                output_path = "output_" + str(x)
+
+            # Copy project template to output
+            shutil.copytree(args.project_template, output_path)
+
+        posts_directory = output_path + "/posts"
+        if not os.path.exists(posts_directory):
+            os.makedirs(posts_directory)
+
+        post_count = 1
+        include_list = ""
+
+        for url in args.uris:
+            if wp_import.check_if_url_is_category(url):
+                print("Found category url: "+url)
+                host,slug = wp_import.get_category_host_slug_from_url(url)
+                category_id = wp_import.get_category_id_from_slug(host,slug)
+                post_slugs = wp_import.get_post_slugs_in_category(host,category_id)
+                for post_slug in post_slugs:
+                    wp_import.download_post(host, post_slug, args, posts_directory, post_count, include_list)
+                    include_list += "\\include{posts/" + "post_" + str(post_count) + "}\n"
+                    post_count = post_count + 1
+            else:
+                host, slug = wp_import.get_host_slug_from_url(url)
+                wp_import.download_post(host, slug, args, posts_directory, post_count, include_list)
+                include_list += "\\include{posts/" + "post_" + str(post_count) + "}\n"
+                post_count = post_count + 1
+
+        # add include_list to main.tex
+        with open(output_path+"/main.tex", "r+") as f:
+            content = f.read()
+            f.seek(0)
+            f.truncate()
+            f.write(content.replace("[wp2latex-file-includes]", include_list))
+
+    else:
+        print("Downloading " + args.uris[0])
+        host, slug = wp_import.get_host_slug_from_url(args.uris[0])
+        post_in_latex = wp_import.generate_post(wp_import.import_post(host, slug, args))
+
+        output_path = ""
+        if args.output:
+            output_path = args.output
+        else:
+            if not os.path.exists("output.tex"):
+                output_path = "output.tex"
+            else:
+                x = 1
+                while os.path.exists("output_" + str(x) + ".tex"):
+                    x = x + 1
+
+                output_path = "output_" + str(x) + ".tex"
+
+        with open(output_path, "x", encoding="utf-8") as f:
+            f.write(post_in_latex)
+
+        print("Saved output to " + output_path)
 
 
 if __name__ == '__main__':
--- wp_import.py
+++ wp_import.py
@@ -6,6 +6,7 @@ import pypandoc
 
 def import_post(host, slug, args):
     print("Slug for post is " + slug + ", host is " + host + ". Trying to get post from API.")
+
     post = requests.get("https://" + host + "/wp-json/wp/v2/posts?slug=" + slug)
 
     if post.status_code != 200:
@@ -43,7 +44,9 @@ def import_post(host, slug, args):
 
             footnote.replace_with("wp2latex-footnote-placeholder-" + str(len(footnotes_dict) - 1))
 
-        soup.find("div", class_="footnotes_reference_container").decompose()
+        frc = soup.find("div", class_="footnotes_reference_container")
+        if frc:
+            frc.decompose()
 
     raw_html = soup.__str__()
     result = pypandoc.convert_text(raw_html, 'tex', format='html', extra_args=["--shift-heading-level-by=-1"])
@@ -74,7 +77,7 @@ def import_post(host, slug, args):
 
 def generate_post(post_data):
     post_template = ""
-    with open("latex-template/single_post.tex", 'r', encoding="utf-8") as f:
+    with open("latex-templates/single_post.tex", 'r', encoding="utf-8") as f:
         post_template = f.read()
 
     authors_string = ""
@@ -87,7 +90,8 @@ def generate_post(post_data):
         .replace("[wp2latex-post-authors]", authors_string) \
         .replace("[wp2latex-post-url]", post_data["link"]) \
         .replace("[wp2latex-post-content]", post_data["content"])
-    print(result)
+
+    return result
 
 
 def get_host_slug_from_url(url):
@@ -95,3 +99,52 @@ def get_host_slug_from_url(url):
     host = parsed_url.netloc
     slug = parsed_url.path[1:-1]
     return host, slug
+
+
+def check_if_url_is_category(url):
+    parsed_url = urllib.parse.urlparse(url)
+    if parsed_url.path.startswith("/category/"):
+        return True
+    else:
+        return False
+
+
+def get_category_host_slug_from_url(url):
+    parsed_url = urllib.parse.urlparse(url)
+    host = parsed_url.netloc
+    raw_slug = parsed_url.path[:-1]
+    slug = raw_slug.split("/")[-1]
+    return host, slug
+
+
+def get_category_id_from_slug(host, slug):
+    category = requests.get("https://" + host + "/wp-json/wp/v2/categories?slug=" + slug)
+
+    if category.status_code != 200:
+        print("Couldn't get category id for URL from API!")
+        return
+
+    print(category.json())
+    return category.json()[0]["id"]
+
+
+def get_post_slugs_in_category(host, category_id):
+    posts = requests.get("https://" + host + "/wp-json/wp/v2/posts?categories=" + str(category_id) + "&per_page=100")
+
+    if posts.status_code != 200:
+        print("Couldn't get posts in category from API!")
+        return
+
+    post_slugs = []
+
+    for post in posts.json():
+        post_slugs.append(post["slug"])
+
+    return post_slugs
+
+
+def download_post(host, slug, args, posts_directory, post_count, include_list):
+    print("Downloading post " + slug)
+    post_in_latex = generate_post(import_post(host, slug, args))
+    with open(posts_directory + "/post_" + str(post_count) + ".tex", "x", encoding="utf-8") as f:
+        f.write(post_in_latex)
