4bfb46a8305aa6f343f665b3e28f0d232c8073b5
Keanu D?lle <ares@anghenfil.de>
2023-07-04 02:50:53 +0200

    FEA: exclude categories recursive; detect more wrong sections

--- wp_import.py
+++ wp_import.py
@@ -51,6 +51,7 @@ def convert_post_json(post_json, args):
     raw_html = soup.__str__()
     result = pypandoc.convert_text(raw_html, 'tex', format='html', extra_args=["--shift-heading-level-by=-1"])
 
+    # TODO: convert footnote text to latex to convert links etc.
     if args.with_footnotes:
         for footnote_count in range(len(footnotes_dict) - 1, -1, -1):
             command_start = ""
@@ -80,6 +81,7 @@ def convert_post_json(post_json, args):
 
     if args.remove_ulines:
         result = result.replace("\\uline", "")
+        result = result.replace("\\ul", "")
 
     result = re.sub(r"(?<!\n)\n(?!\n)", " ", result)
     post_data["content"] = result
@@ -170,7 +172,8 @@ def get_category_id_from_slug(host, slug):
     return category.json()[0]["id"]
 
 
-def get_all_posts(host, categories, exclude_categories, after, before, page=1):
+def get_all_posts(host, categories, exclude_categories, after, before, recursive_exclude_categories=False, page=1):
+    global parent_category
     query = "https://" + host + "/wp-json/wp/v2/posts"
     first_argument = True
 
@@ -209,15 +212,42 @@ def get_all_posts(host, categories, exclude_categories, after, before, page=1):
 
     print("Doing request to API target " + query)
     raw_posts = requests.get(query)
-    if requests.status_codes != 200:
+    if raw_posts.status_code != 200:
         print("Couldn't get posts from API: " + raw_posts.text)
+        return
 
     total_pages = int(raw_posts.headers.get("X-WP-TotalPages"))
 
     posts = raw_posts.json()
 
     if page < total_pages:
-        posts += get_all_posts(host, categories, after, before, page + 1)
+        posts += get_all_posts(host, categories, after, before, page + 1, recursive_exclude_categories)
+
+    print("Got " + str(len(posts)) + " posts.")
+
+    if recursive_exclude_categories:
+        new_posts = []
+        for post in posts:
+            categories = post["categories"]
+
+            excluded_category_found = False
+            for category in categories:
+                print("Getting parent categories for post " + post["slug"])
+                parent_categories = get_parent_categories(host, category)
+                print("parent categories:" + str(parent_categories))
+                for parent_category in parent_categories:
+                    print("Test:" + str(exclude_categories.split(",")))
+                    if str(parent_category) in exclude_categories.split(","):
+                        print("Found post where parent category is excluded category, removing " + post["slug"])
+                        excluded_category_found = True
+                        break
+                if excluded_category_found:
+                    break
+
+            if not excluded_category_found:
+                new_posts.append(post)
+
+        posts = new_posts
     return posts
 
 
@@ -230,7 +260,9 @@ def download_post(host, slug, args, posts_directory, post_count):
 
 def fix_sections(input_str):
     regex = r"\\hypertarget\{[^}]+\}\{%\n\\section{\\texorpdfstring{\\textbf\{[^}]+\}}\{[^}]+\}\}\\label\{[^}]+\}}"
+    regex2 = r"\\hypertarget\{[^}]+\}\{%\n\\section{[^}]+\}\\label\{[^}]+\}}"
     matches = re.findall(regex, input_str)
+    matches2 = re.findall(regex2, input_str)
 
     for match in matches:
         old_section_command = match
@@ -238,9 +270,34 @@ def fix_sections(input_str):
         section_title = re.search(regex2, old_section_command).group(1)
         input_str = input_str.replace(old_section_command, "\\section*{" + tex_escape(section_title) + "}")
 
+    for match in matches2:
+        old_section_command = match
+        regex2 = r"\\section{([^{}]+)"
+        section_title = re.search(regex2, old_section_command).group(1)
+        input_str = input_str.replace(old_section_command, "\\section*{" + tex_escape(section_title) + "}")
+
     input_str = input_str.replace("\\section{", "\\section*{")
 
     return input_str
+
+
+def get_parent_categories(host, category_id):
+    category = requests.get("https://" + host + "/wp-json/wp/v2/categories/" + str(category_id))
+    print("Requesting category " + "https://" + host + "/wp-json/wp/v2/categories/" + str(category_id))
+    if category.status_code != 200 or len(category.json()) == 0:
+        print("Couldn't get category id for URL from API!")
+        return []
+
+    category = category.json()
+    parent_categories = []
+
+    if category["parent"] != 0:
+        parent_categories.append(category["parent"])
+        print("Found parent category:" + str(category["parent"]) + ", current categories: " + str(parent_categories))
+        parent_categories += get_parent_categories(host, category["parent"])
+    else:
+        print("Category " + str(category_id) + " has no parents. Returning " + str(parent_categories))
+    return parent_categories
 
 
 def tex_escape(text):
